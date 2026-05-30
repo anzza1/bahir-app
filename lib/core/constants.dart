bdc4f950e5ae3a69f8358f09915434e324106523
anzza1 <anzza1@github.com>
2026-05-30 12:03:13 +0100

    fix: use WiFi IP instead of localhost

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
-const String kServerIp = '127.0.0.1';
+const String kServerIp = '192.168.1.107';
 const String kApiBase  = 'http://$kServerIp:8080/api';
 const String kWsBase   = 'ws://$kServerIp:8081/ws';
 const Duration kTimeout = Duration(seconds: 10);
